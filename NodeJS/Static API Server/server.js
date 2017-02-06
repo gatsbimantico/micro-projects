35b4a59d645b769c29714bce4d5c472bead0ef6a
cesarccarrera <cesar.c-carrera@tribalworldwide.co.uk>
2017-02-06 11:30:51 +0000

    Add cross origin and log

--- NodeJS/Static API Server/server.js
+++ NodeJS/Static API Server/server.js
@@ -1,12 +1,23 @@
 require('http')
     .createServer((req, res) => {
-        var json;
+        if (req.url == '/favicon.ico') return;
+
+        var json,
+            route = '.' + req.url;
+        console.log(req.method + ' ' + route);
+
         try {
-            json = require('fs').readFileSync('.' + req.url);
+            json = require('fs').readFileSync(route);
         } catch (e) {
             json = '{"status":"error"}';
         }
-        res.writeHead(200, { 'Content-Type': 'application/json' });
+
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'PUT, GET, POST, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+        });
         res.end(json);
     })
     .listen(3000, 'localhost');
